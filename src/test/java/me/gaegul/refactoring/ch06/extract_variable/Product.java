a8751d9a4969a5f8c6c8d3cc8626c6d01ba3acf6
gaegulgaegul <dla9721@gmail.com>
2023-03-21 23:29:45 +0900

    refactor 변수 추출하기 - 수량 할인 추출

--- src/test/java/me/gaegul/refactoring/ch06/extract_variable/Product.java
+++ src/test/java/me/gaegul/refactoring/ch06/extract_variable/Product.java
@@ -3,8 +3,7 @@ package me.gaegul.refactoring.ch06.extract_variable;
 public class Product {
     public double price(Order order) {
         int basePrice = order.getQuantity() * order.getItemPrice();
-        return basePrice
-                - Math.max(0, order.getQuantity() - 500) * order.getItemPrice() * 0.05
-                + Math.min(basePrice * 0.1, 100);
+        double quantityDiscount = Math.max(0, order.getQuantity() - 500) * order.getItemPrice() * 0.05;
+        return basePrice - quantityDiscount + Math.min(basePrice * 0.1, 100);
     }
 }
