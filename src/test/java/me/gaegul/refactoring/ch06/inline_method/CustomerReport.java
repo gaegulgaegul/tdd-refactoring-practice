d1ba53a3267b1dc0fb6a3b8dc2f63346f9ed3959
gaegulgaegul <dla9721@gmail.com>
2023-03-21 23:19:28 +0900

    refactor 함수 인라인하기 - 지역 정보 추가 기능 인라인 리팩터링

--- src/test/java/me/gaegul/refactoring/ch06/inline_method/CustomerReport.java
+++ src/test/java/me/gaegul/refactoring/ch06/inline_method/CustomerReport.java
@@ -7,11 +7,8 @@ public class CustomerReport {
     public List<List<String>> reportLines(Customer aCustomer) {
         List<List<String>> lines = new ArrayList<>();
         lines.add(List.of("name", aCustomer.getName()));
-        gatherCustomerData(lines, aCustomer);
+        lines.add(List.of("location", aCustomer.getLocation()));
         return lines;
     }
 
-    private void gatherCustomerData(List<List<String>> lines, Customer aCustomer) {
-        lines.add(List.of("location", aCustomer.getLocation()));
-    }
 }
