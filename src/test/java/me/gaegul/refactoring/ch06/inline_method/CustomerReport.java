a5be2a3834f271d467c945324f20f74a5ce60e5d
gaegulgaegul <dla9721@gmail.com>
2023-03-21 23:19:13 +0900

    refactor 함수 인라인하기 - 이름 정보 추가 기능 인라인 리팩터링

--- src/test/java/me/gaegul/refactoring/ch06/inline_method/CustomerReport.java
+++ src/test/java/me/gaegul/refactoring/ch06/inline_method/CustomerReport.java
@@ -6,12 +6,12 @@ import java.util.List;
 public class CustomerReport {
     public List<List<String>> reportLines(Customer aCustomer) {
         List<List<String>> lines = new ArrayList<>();
+        lines.add(List.of("name", aCustomer.getName()));
         gatherCustomerData(lines, aCustomer);
         return lines;
     }
 
     private void gatherCustomerData(List<List<String>> lines, Customer aCustomer) {
-        lines.add(List.of("name", aCustomer.getName()));
         lines.add(List.of("location", aCustomer.getLocation()));
     }
 }
