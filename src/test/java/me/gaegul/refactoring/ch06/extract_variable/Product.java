06e8921ca8baa3ba64cd2cc9b31f6500b6640a05
gaegulgaegul <dla9721@gmail.com>
2023-03-21 23:35:16 +0900

    refactor 클래스 문맥에서 처리 - 가격 계산 기능 메서드 추출

--- src/test/java/me/gaegul/refactoring/ch06/extract_variable/Product.java
+++ src/test/java/me/gaegul/refactoring/ch06/extract_variable/Product.java
@@ -2,9 +2,21 @@ package me.gaegul.refactoring.ch06.extract_variable;
 
 public class Product {
     public double price(Order order) {
-        int basePrice = order.getQuantity() * order.getItemPrice();
-        double quantityDiscount = Math.max(0, order.getQuantity() - 500) * order.getItemPrice() * 0.05;
-        double shipping = Math.min(basePrice * 0.1, 100);
+        int basePrice = getBasePrice(order);
+        double quantityDiscount = getQuantityDiscount(order);
+        double shipping = getShipping(order);
         return basePrice - quantityDiscount + shipping;
+    }
+
+    private double getShipping(Order order) {
+        return Math.min(getBasePrice(order) * 0.1, 100);
+    }
+
+    private double getQuantityDiscount(Order order) {
+        return Math.max(0, order.getQuantity() - 500) * order.getItemPrice() * 0.05;
+    }
+
+    private int getBasePrice(Order order) {
+        return order.getQuantity() * order.getItemPrice();
     }
 }
