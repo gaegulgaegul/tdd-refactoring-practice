00bb4c18d1df11829498aa00277cd8fcb6c56532
gaegulgaegul <dla9721@gmail.com>
2023-03-21 23:27:48 +0900

    refactor 변수 추출하기 - 기본 가격 추출

--- src/test/java/me/gaegul/refactoring/ch06/extract_variable/Product.java
+++ src/test/java/me/gaegul/refactoring/ch06/extract_variable/Product.java
@@ -2,7 +2,8 @@ package me.gaegul.refactoring.ch06.extract_variable;
 
 public class Product {
     public double price(Order order) {
-        return order.getQuantity() * order.getItemPrice()
+        int basePrice = order.getQuantity() * order.getItemPrice();
+        return basePrice
                 - Math.max(0, order.getQuantity() - 500) * order.getItemPrice() * 0.05
                 + Math.min(order.getQuantity() * order.getItemPrice() * 0.1, 100);
     }
