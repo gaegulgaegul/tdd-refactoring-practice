6838f6f6eeacce2b611c7e7a0da7ce3800b6bdb1
gaegulgaegul <dla9721@gmail.com>
2023-03-21 23:36:29 +0900

    refactor 클래스 문맥에서 처리 - 임시 변수 인라인

--- src/test/java/me/gaegul/refactoring/ch06/extract_variable/Product.java
+++ src/test/java/me/gaegul/refactoring/ch06/extract_variable/Product.java
@@ -2,10 +2,7 @@ package me.gaegul.refactoring.ch06.extract_variable;
 
 public class Product {
     public double price(Order order) {
-        int basePrice = order.getBasePrice();
-        double quantityDiscount = order.getQuantityDiscount();
-        double shipping = order.getShipping();
-        return basePrice - quantityDiscount + shipping;
+        return order.getBasePrice() - order.getQuantityDiscount() + order.getShipping();
     }
 
 }
