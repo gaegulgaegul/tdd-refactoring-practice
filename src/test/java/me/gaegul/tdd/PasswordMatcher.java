0a1253c80813b7e595970b6f8a963f5f25f3343c
gaegulgaegul <dla9721@gmail.com>
2023-03-27 23:02:59 +0900

    feat 암호는 모든 규칙을 만족하면 강함 상태를 반환한다.

--- src/test/java/me/gaegul/tdd/PasswordMatcher.java
+++ src/test/java/me/gaegul/tdd/PasswordMatcher.java
@@ -2,6 +2,9 @@ package me.gaegul.tdd;
 
 public class PasswordMatcher {
     public PasswordStrength match(String password) {
-        throw new IllegalArgumentException();
+        if (password == null || "".equals(password)) {
+            throw new IllegalArgumentException();
+        }
+        return PasswordStrength.STRONG;
     }
 }
