017008378681488068dde70d6baea4d34dbabdb4
gaegulgaegul <dla9721@gmail.com>
2023-03-27 23:10:58 +0900

    feat 암호는 길이가 8글자 이상이고 0~9숫자를 포함하면 보통 상태를 반환한다.

--- src/test/java/me/gaegul/tdd/PasswordMatcher.java
+++ src/test/java/me/gaegul/tdd/PasswordMatcher.java
@@ -1,9 +1,16 @@
 package me.gaegul.tdd;
 
+import java.util.regex.Pattern;
+
 public class PasswordMatcher {
     public PasswordStrength match(String password) {
         if (password == null || "".equals(password)) {
             throw new IllegalArgumentException();
+        }
+        Pattern pattern = Pattern.compile("[0-9]");
+
+        if (password.length() >= 8 && pattern.matcher(password).find()) {
+            return PasswordStrength.NORMAL;
         }
         return PasswordStrength.STRONG;
     }
