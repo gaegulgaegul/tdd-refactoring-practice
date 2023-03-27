ace1ed0c72a61c8c49197ddcdb866ca94b68bb41
gaegulgaegul <dla9721@gmail.com>
2023-03-27 22:52:43 +0900

    test 암호 강도 측정 기능 null 확인 테스트

---
+++ src/test/java/me/gaegul/tdd/PasswordMatcher.java
@@ -0,0 +1,6 @@
+package me.gaegul.tdd;
+
+public class PasswordMatcher {
+    public void match(String password) {
+    }
+}
---
+++ src/test/java/me/gaegul/tdd/PasswordMatcherTest.java
@@ -0,0 +1,18 @@
+package me.gaegul.tdd;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@DisplayName("패스워드 강도 측정 기능 테스트")
+public class PasswordMatcherTest {
+
+    @DisplayName("null이면 예외발생")
+    @Test
+    void null_check() {
+        PasswordMatcher matcher = new PasswordMatcher();
+        assertThatThrownBy(() -> matcher.match(null))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
