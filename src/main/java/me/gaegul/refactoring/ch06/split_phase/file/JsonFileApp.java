24f67ea8585f270d11327df31df3a6ed9d166e31
gaegulgaegul <dla9721@gmail.com>
2023-04-19 22:05:47 +0900

    feat 다른 예시를 위해 되돌리기

--- src/main/java/me/gaegul/refactoring/ch06/split_phase/file/JsonFileApp.java
+++ src/main/java/me/gaegul/refactoring/ch06/split_phase/file/JsonFileApp.java
@@ -18,24 +18,13 @@ public class JsonFileApp {
 	}
 
 	static long run(String[] args) throws IOException {
-		CommandLine commandLine = parseCommandLine(args);
-		return countOrders(commandLine, args);
-	}
-
-	private static CommandLine parseCommandLine(String[] args) {
 		if (args.length == 0) throw new RuntimeException("파일명을 입력하세요.");
-		CommandLine result = new CommandLine();
-		result.onlyCountReady = Stream.of(args).anyMatch(arg -> "-r".equals(arg));
-		result.filename = args[args.length - 1];
-		return result;
-	}
-
-	private static long countOrders(CommandLine commandLine, String[] args) throws IOException {
-		File input = Paths.get(commandLine.filename).toFile();
+		String filename = args[args.length - 1];
+		File input = Paths.get(filename).toFile();
 		ObjectMapper mapper = new ObjectMapper();
 		Order[] orders = mapper.readValue(input, Order[].class);
 
-		if (commandLine.onlyCountReady) {
+		if (Stream.of(args).anyMatch(arg -> "-r".equals(arg))) {
 			return Stream.of(orders)
 				.filter(o -> "ready".equals(o.status))
 				.count();
@@ -47,16 +36,10 @@ public class JsonFileApp {
 	static class Order {
 		String status;
 
-		Order() {
-		}
+		Order() {}
 
 		void setStatus(final String status) {
 			this.status = status;
 		}
-	}
-
-	private static class CommandLine {
-		String filename;
-		boolean onlyCountReady;
 	}
 }
