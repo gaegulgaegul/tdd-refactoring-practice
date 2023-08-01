10fac12e1099e1b7dd140754840fb2a0c8be6fff
gaegulgaegul <dla9721@gmail.com>
2023-08-01 21:15:34 +0900

    refactor 여러 함수를 클래스로 묶기

--- src/test/java/me/gaegul/refactoring/ch10/RatingService.java
+++ src/test/java/me/gaegul/refactoring/ch10/RatingService.java
@@ -10,69 +10,96 @@ public class RatingService {
      * @param history
      * @return
      */
-    public String rating(Voyage voyage, List<History> history) {
-        int vpf = voyageProfitFactor(voyage, history);
-        int vr = voyageRisk(voyage);
-        int chr = captainHistoryRisk(voyage, history);
-        if (vpf * 3 > (vr + chr * 2)) return "A";
-        return "B";
+    public String rating(final Voyage voyage, final List<History> history) {
+        return new Rating(voyage, history).getValue();
     }
 
-    /**
-     * 항해 경로 위험요소
-     * @param voyage
-     * @return
-     */
-    private int voyageRisk(Voyage voyage) {
-        int result = 1;
-        if (voyage.length() > 4) result += 2;
-        if (voyage.length() > 8) result += voyage.length() - 8;
-        if (List.of("중국", "동인도").contains(voyage.getZone())) result += 4;
-        return Math.max(result, 0);
-    }
+    public static class Rating {
+        private final Voyage voyage;
+        private final List<History> history;
 
-    /**
-     * 선장의 항해 이력 위험요소
-     * @param voyage
-     * @param history
-     * @return
-     */
-    private int captainHistoryRisk(Voyage voyage, List<History> history) {
-        int result = 1;
-        if (history.size() < 5) result += 4;
-        result += history.stream().filter(item -> item.getProfit() < 0).count();
-        if ("중국".equals(voyage.getZone()) && hasChina(history)) result -= 2;
-        return Math.max(result, 0);
-    }
-
-    /**
-     * 중국을 경유하는가?
-     * @param history
-     * @return
-     */
-    private boolean hasChina(List<History> history) {
-        return history.stream().anyMatch(item -> "중국".equals(item.getZone()));
-    }
-
-    /**
-     * 수익 요인
-     * @param voyage
-     * @param history
-     * @return
-     */
-    private int voyageProfitFactor(Voyage voyage, List<History> history) {
-        int result = 2;
-        if ("중국".equals(voyage.getZone())) result += 1;
-        if ("동인도".equals(voyage.getZone())) result += 1;
-        if ("중국".equals(voyage.getZone()) && hasChina(history)) {
-            result += 3;
-            if (history.size() > 10) result += 1;
-            if (voyage.length() > 12) result += 1;
-            if (voyage.length() > 18) result -= 1;
-        } else {
-            if (history.size() > 8) result += 1;
-            if (voyage.length() > 14) result -= 1;
+        public Rating(final Voyage voyage, final List<History> history) {
+            this.voyage = voyage;
+            this.history = history;
         }
-        return result;
+
+        private String getValue() {
+            final int vpf = voyageProfitFactor(this.voyage, this.history);
+            final int vr = voyageRisk(this.voyage);
+            final int chr = captainHistoryRisk(this.voyage, this.history);
+            if (vpf * 3 > (vr + chr * 2))
+                return "A";
+            return "B";
+        }
+
+        /**
+         * 항해 경로 위험요소
+         * @param voyage
+         * @return
+         */
+        private int voyageRisk(final Voyage voyage) {
+            int result = 1;
+            if (voyage.length() > 4)
+                result += 2;
+            if (voyage.length() > 8)
+                result += voyage.length() - 8;
+            if (List.of("중국", "동인도").contains(voyage.getZone()))
+                result += 4;
+            return Math.max(result, 0);
+        }
+
+        /**
+         * 선장의 항해 이력 위험요소
+         * @param voyage
+         * @param history
+         * @return
+         */
+        private int captainHistoryRisk(final Voyage voyage, final List<History> history) {
+            int result = 1;
+            if (history.size() < 5)
+                result += 4;
+            result += history.stream().filter(item -> item.getProfit() < 0).count();
+            if ("중국".equals(voyage.getZone()) && hasChina(history))
+                result -= 2;
+            return Math.max(result, 0);
+        }
+
+        /**
+         * 중국을 경유하는가?
+         * @param history
+         * @return
+         */
+        private boolean hasChina(final List<History> history) {
+            return history.stream().anyMatch(item -> "중국".equals(item.getZone()));
+        }
+
+        /**
+         * 수익 요인
+         * @param voyage
+         * @param history
+         * @return
+         */
+        private int voyageProfitFactor(final Voyage voyage, final List<History> history) {
+            int result = 2;
+            if ("중국".equals(voyage.getZone()))
+                result += 1;
+            if ("동인도".equals(voyage.getZone()))
+                result += 1;
+            if ("중국".equals(voyage.getZone()) && hasChina(history)) {
+                result += 3;
+                if (history.size() > 10)
+                    result += 1;
+                if (voyage.length() > 12)
+                    result += 1;
+                if (voyage.length() > 18)
+                    result -= 1;
+            } else {
+                if (history.size() > 8)
+                    result += 1;
+                if (voyage.length() > 14)
+                    result -= 1;
+            }
+            return result;
+        }
     }
 }
