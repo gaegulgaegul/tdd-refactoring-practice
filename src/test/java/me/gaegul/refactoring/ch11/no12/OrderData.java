4dd7e1e511f74fee312ac9793822a34653b5e06d
gaegulgaegul <dla9721@gmail.com>
2023-09-20 23:27:23 +0900

    feat calculateShippingCosts 기능 추가

---
+++ src/test/java/me/gaegul/refactoring/ch11/no12/OrderData.java
@@ -0,0 +1,13 @@
+package me.gaegul.refactoring.ch11.no12;
+
+public class OrderData {
+	private String country;
+
+	public OrderData(final String country) {
+		this.country = country;
+	}
+
+	public String country() {
+		return this.country;
+	}
+}
--- src/test/java/me/gaegul/refactoring/ch11/no12/ReplaceErrorCodeWithException.java
+++ src/test/java/me/gaegul/refactoring/ch11/no12/ReplaceErrorCodeWithException.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 public class ReplaceErrorCodeWithException {
 
-	public static final Map<String, Integer> COUNTRY_DATA = new HashMap<>();
+	private static final Map<String, Integer> COUNTRY_DATA = new HashMap<>();
 
 	static {
 		COUNTRY_DATA.put("대한민국", 33);
@@ -15,5 +15,11 @@ public class ReplaceErrorCodeWithException {
 		Integer data = COUNTRY_DATA.get(country);
 		if (data != null) return data.intValue();
 		return -23;
+	}
+
+	public int calculateShippingCosts(OrderData order) {
+		int shippingRules = localShippingRules(order.country());
+		if (shippingRules < 0) return shippingRules;
+		return shippingRules;
 	}
 }
