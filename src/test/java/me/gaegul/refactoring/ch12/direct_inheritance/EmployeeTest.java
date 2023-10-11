b71f0ef41840c45ddff29ebf66810b16577d1b54
gaegulgaegul <dla9721@gmail.com>
2023-10-11 21:36:56 +0900

    test Employee#isNotDefinedEmployeeType 테스트 코드 추가

--- src/test/java/me/gaegul/refactoring/ch12/direct_inheritance/EmployeeTest.java
+++ src/test/java/me/gaegul/refactoring/ch12/direct_inheritance/EmployeeTest.java
@@ -1,5 +1,6 @@
 package me.gaegul.refactoring.ch12.direct_inheritance;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
@@ -19,30 +20,39 @@ class EmployeeTest {
 		class validateType_메서드는 {
 
 			@Test
-			void 엔지니어_타입을_가지는_직원이면_예외를_발생하지_않는다() {
-				final Employee employee = new Employee("엔지니어", "engineer");
-				employee.validateType();
-			}
-
-			@Test
-			void 매니저_타입을_가지는_직원이면_예외를_발생하지_않는다() {
-				final Employee employee = new Employee("매니저", "manager");
-				employee.validateType();
-			}
-
-			@Test
-			void 영업사원_타입을_가지는_직원이면_예외를_발생하지_않는다() {
-				final Employee employee = new Employee("영업사원", "salesperson");
-				employee.validateType();
-			}
-
-			@Test
 			void 프로그래머_타입을_가지는_직원이면_예외가_발생한다() {
 				final Employee employee = new Employee("프로그래머", "programmer");
 				assertThatThrownBy(employee::validateType)
 					.isInstanceOf(IllegalArgumentException.class);
 			}
 		}
-	}
 
+		@Nested
+		class isNotDefinedEmployeeType_메서드는 {
+
+			@Test
+			void 엔지니어_타입을_가지는_직원이면_false를_반환한다() {
+				final Employee employee = new Employee("엔지니어", "engineer");
+				assertThat(employee.isNotDefinedEmployeeType()).isFalse();
+			}
+
+			@Test
+			void 매니저_타입을_가지는_직원이면_false를_반환한다() {
+				final Employee employee = new Employee("매니저", "manager");
+				assertThat(employee.isNotDefinedEmployeeType()).isFalse();
+			}
+
+			@Test
+			void 영업사원_타입을_가지는_직원이면_false를_반환한다() {
+				final Employee employee = new Employee("영업사원", "salesperson");
+				assertThat(employee.isNotDefinedEmployeeType()).isFalse();
+			}
+
+			@Test
+			void 프로그래머_타입을_가지는_직원이면_true를_반환한다() {
+				final Employee employee = new Employee("프로그래머", "programmer");
+				assertThat(employee.isNotDefinedEmployeeType()).isTrue();
+			}
+		}
+	}
 }
